413abc8eedd57103bb1e069dc09f5c72d57c8e89
Pavlos Polianidis <ppoliani@gmail.com>
2017-07-21 19:46:26 +0100

    add Proof contract

---
+++ src/solidity/Proof.sol
@@ -0,0 +1,32 @@
+pragma Solidity ^0.4.13;
+
+contract Proof {
+  struct FileDetails {
+    uint timestamp;
+    string owner;
+  }
+
+  mapping (string => FileDetails) files;
+
+  event logFileAddedStatus(bool status, uint timestamp, string owner, string filehash);
+
+  //this is used to store the owner of file at the block timestamp
+  function set(string owner, string filehash) {
+    //There is no proper way to check if a key already exists or not therefore
+    // we are checking for default value i.e., all bits are 0
+    if(files[filehash].timestamp == 0) {
+      files[filehash] = FileDetails(block.timestamp, owner);
+
+      // raise the event so that everyone knows about this new record
+      logFileAddedStatus(true, block.timestamp, owner, filehash);
+    }
+    else {
+      // status false because ownership for the specific file already exist in the blockchain ledger
+      logFileAddedStatus(true, block.timestamp, owner, filehash);
+    }
+  }
+
+  function get(string filehash) return (uint timestamp, string owner) {
+    return (files[filehash].timestamp, files[filehash].owner);
+  }
+}
