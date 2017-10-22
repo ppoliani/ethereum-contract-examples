6646f1459fef018492d0b88017c6de2395fda489
ppoliani <ppoliani@gmail.com>
2017-10-22 18:20:29 +0100

    remove unused truffle project

--- src/solidity/aroracoin/contracts/TokenRecipient.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.11;
-
-// an external contract that will interact with the AroraToken Contract
-// because contracts aren't able to subscribe to Events only to function calls
-// This is a contract that any third party who want to interact with the Arora tower,
-// needs to deploy.
-contract TokenRecipient {
-  function receiveApproval(address from, uint256 value, address token, bytes extraData);
-}
--- src/solidity/aroracoin/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const AroraToken = artifacts.require("./AroraToken.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(AroraToken);
-};
